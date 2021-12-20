55f9d9f562f358c2faa06c3e0517cbb9d9b0cf4a
anshultaklikar <anshultaklikar8@gmail.com>
2021-12-20 21:48:47 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'hotreload';
+
+  show: boolean = false;
+
+  skills: string[] = ['Angular', 'React', 'Vue'];
+
+  empData: any[] = [
+    { id: 666, name: 'Raj', age: 44 },
+    { id: 665, name: 'Raju', age: 43 },
+    { id: 667, name: 'Raja', age: 42 },
+  ];
+
+  handleShow() {
+    this.show = true;
+  }
+  handleHide() {
+    this.show = false;
+  }
+  handleToggle() {
+    this.show ? (this.show = false) : (this.show = true);
+  }
 }
